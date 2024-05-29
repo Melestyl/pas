f1a20811ac875a1091b364c14bd24d7e6118f9c8
Melestyl <tom.telcal@icloud.com>
2024-05-29 23:49:49 +0200

    create_area now returns a segment (int), corresponding to a shared memory

--- Server/server.c
+++ Server/server.c
@@ -3,18 +3,22 @@
 #define STR_LARGE 100
 #define STR_SMALL 50
 
-area_t* create_area(enum type type, char* name) {
-	static unsigned shared_memory_id = 0;
-	char shared_memory_prefix[STR_SMALL], project_id_str[STR_SMALL], shared_memory_path[STR_LARGE];
-	int project_id;
+int create_area(enum type type, char* name) {
+	static unsigned project_id = 1;
+	char shared_memory_path[STR_LARGE];
 	key_t token;
+	int shared_memory_segment;
+	area_t *area;
 
-	// Allocating memory for area
-	area_t* area = (area_t*) malloc(sizeof(area_t));
-	if (area == NULL) {
-		perror("malloc");
-		exit(1);
-	}
+	// Retrieving shared memory path
+	read_config("../Common/config.txt", "SHARED_MEMORY_PATH", shared_memory_path);
+
+	// Initializing shared memory
+	token = ftok(shared_memory_path, project_id++);
+	shared_memory_segment = shmget(token, sizeof(area_t), IPC_CREAT | 0666);
+
+	// Attaching shared memory
+	area = (area_t*) shmat(shared_memory_segment, NULL, 0);
 
 	// Setting type
 	area->type = type;
@@ -22,27 +26,47 @@ area_t* create_area(enum type type, char* name) {
 	// Setting name
 	strcpy(area->name, name);
 
-	// Retrieving shared memory path and project id
-	read_config("../Common/config.txt", "SHARED_MEMORY_PREFIX", shared_memory_prefix);
-	project_id = atoi(read_config("../Common/config.txt", "PROJECT_ID", project_id_str));
-
-	// Initializing shared memory
-	sprintf(shared_memory_path, "%s%d", shared_memory_prefix, shared_memory_id++);
-	token = ftok(shared_memory_path, project_id);
-	area->shared_memory = shmget(token, sizeof(int), IPC_CREAT | 0666);
-	if (area->shared_memory == -1) {
-		perror("shmget");
-		exit(1);
-	}
-	strcpy(area->path, shared_memory_path);
-
 	// Initializing mutex
 	pthread_mutex_init(&area->mutex, NULL);
 
-	// Returning area's pointer
-	return area;
+	// Returning shared memory segment
+	return shared_memory_segment;
+}
+
+void print_shared_memory_info(int shared_memory) {
+	struct shmid_ds shmid_ds;
+	if (shmctl(shared_memory, IPC_STAT, &shmid_ds) == -1) {
+		perror("shmctl");
+		exit(1);
+	}
+
+	printf("Shared memory info:\n");
+	printf("  - Segment ID: %d\n", shared_memory);
+	printf("  - Size: %ld\n", shmid_ds.shm_segsz);
+	printf("  - Last attach time: %ld\n", shmid_ds.shm_atime);
+	printf("  - Last detach time: %ld\n", shmid_ds.shm_dtime);
+	printf("  - Last change time: %ld\n", shmid_ds.shm_ctime);
+	printf("  - Number of attaches: %ld\n", shmid_ds.shm_nattch);
 }
 
 int main() {
+	// Creating areas
+	int desk = create_area(DESK, "Desk-001");
+	int meeting_room = create_area(MEETING_ROOM, "Meeting-Room-001");
+
+	// Printing shared memory info
+	print_shared_memory_info(desk);
+	print_shared_memory_info(meeting_room);
+
+	// Deleting shared memory
+	if (shmctl(desk, IPC_RMID, NULL) == -1) {
+		perror("shmctl");
+		exit(1);
+	}
+	if (shmctl(meeting_room, IPC_RMID, NULL) == -1) {
+		perror("shmctl");
+		exit(1);
+	}
+
 	return 0;
 }
