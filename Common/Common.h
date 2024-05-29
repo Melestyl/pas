0e3a9cea679322e0b218ee95618b433983a0f814
Anael DELANNOY <anaeldelannoy@gmail.com>
2024-05-29 17:08:39 +0200

    Created structure area with mutex ans shared memory

---
+++ Common/Common.h
@@ -0,0 +1,12 @@
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <string.h>
+
+#include <sys/shm.h>
+#include <errno.h>
+
+struct area {
+    pthread_mutex_t mutex;
+    int shared_memory;    
+};
