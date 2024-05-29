de84b2f2f3b19fbebe3a76291089ea39b1073097
Melestyl <tom.telcal@icloud.com>
2024-05-30 01:04:55 +0200

    Moved a part of server.c in areas.c

--- Server/server.c
+++ Server/server.c
@@ -1,37 +1,8 @@
 #include "server.h"
+#include "areas.h"
 
-#define STR_LARGE 100
-#define STR_SMALL 50
-
-int create_area(enum type type, char* name) {
-	static unsigned project_id = 1;
-	char shared_memory_path[STR_LARGE];
-	key_t token;
-	int shared_memory_segment;
-	area_t *area;
-
-	// Retrieving shared memory path
-	read_config("../Common/config.txt", "SHARED_MEMORY_PATH", shared_memory_path);
-
-	// Initializing shared memory
-	token = ftok(shared_memory_path, project_id++);
-	shared_memory_segment = shmget(token, sizeof(area_t), IPC_CREAT | 0666);
-
-	// Attaching shared memory
-	area = (area_t*) shmat(shared_memory_segment, NULL, 0);
-
-	// Setting type
-	area->type = type;
-
-	// Setting name
-	strcpy(area->name, name);
-
-	// Initializing mutex
-	pthread_mutex_init(&area->mutex, NULL);
-
-	// Returning shared memory segment
-	return shared_memory_segment;
-}
+// Global areas list
+node_t *areas_list = NULL;
 
 void print_shared_memory_info(int shared_memory) {
 	struct shmid_ds shmid_ds;
@@ -52,21 +23,20 @@ void print_shared_memory_info(int shared_memory) {
 int main() {
 	// Creating areas
 	int desk = create_area(DESK, "Desk-001");
+	areas_list = add_area_to_list(areas_list, desk);
 	int meeting_room = create_area(MEETING_ROOM, "Meeting-Room-001");
+	areas_list = add_area_to_list(areas_list, meeting_room);
 
 	// Printing shared memory info
 	print_shared_memory_info(desk);
 	print_shared_memory_info(meeting_room);
 
-	// Deleting shared memory
-	if (shmctl(desk, IPC_RMID, NULL) == -1) {
-		perror("shmctl");
-		exit(1);
-	}
-	if (shmctl(meeting_room, IPC_RMID, NULL) == -1) {
-		perror("shmctl");
-		exit(1);
-	}
+	// Listing areas
+	print_list(areas_list);
+
+	// Removing areas
+	areas_list = remove_area_from_list(areas_list, desk);
+	areas_list = remove_area_from_list(areas_list, meeting_room);
 
 	return 0;
 }
