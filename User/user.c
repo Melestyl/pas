d90a9b79008c600931ce6760fc32d47c2f678f7c
Melestyl <tom.telcal@icloud.com>
2024-06-03 19:18:21 +0200

    Book area

--- User/user.c
+++ User/user.c
@@ -94,8 +94,82 @@ void show_areas() {
 	}
 }
 
+/**
+ * @brief Returns an area if it exists in the list
+ * @param name: name of the area to search
+ * @param head: head of the list
+ * @return node_t: the area if it exists, NULL otherwise
+ */
+node_t * search_for_area(char* name, node_t * head) {
+	node_t * temp = head;
+	area_t * area;
+
+	while(temp != NULL) {
+		// Attaching shared memory
+		area = (area_t *) shmat(temp->data, NULL, 0);
+		if(area == (area_t *)-1) {
+			perror("shmat");
+			exit(1);
+		}
+
+		// Comparing names of areas and if found, detaching shared memory and returning 1
+		if(strcmp(area->name, name) == 0) {
+			if(shmdt(area) == -1) {
+				perror("shmdt");
+				exit(1);
+			}
+			return temp;
+		}
+
+		// Detaching shared memory and moving to the next node
+		if(shmdt(area) == -1) {
+			perror("shmdt");
+			exit(1);
+		}
+		temp = temp->next;
+	}
+	return NULL;
+}
+
 void book_area() {
-    printf("Réserver un earea\n");
+	char name[LENGTH_NAME_AREA];
+	node_t * temp = area_list;
+	area_t * area;
+	bool found = false;
+
+	printf("Quel emplacement voulez-vous réserver ? ");
+	scanf("%s", name);
+
+	// Searching for the area
+	temp = search_for_area(name, area_list);
+	if(temp == NULL) {
+		printf("Cet emplacement n'existe pas\n");
+		return;
+	}
+
+	// Attaching shared memory
+	area = (area_t *) shmat(temp->data, NULL, 0);
+	if(area == (area_t *)-1) {
+		perror("shmat");
+		exit(1);
+	}
+
+	// Checking if the area is already reserved
+	if(area->shared_memory != 0) {
+		printf("Cet emplacement est déjà réservé\n");
+		return;
+	}
+	// Reserving the area
+	else {
+		area->shared_memory = getpid();
+		printf("Emplacement réservé avec succès\n");
+	}
+
+	// Detaching shared memory
+	if(shmdt(area) == -1) {
+		perror("shmdt");
+		exit(1);
+	}
 }
 
 void return_area() {
